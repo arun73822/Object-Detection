ccdf1859cb0a7152a55c28e6ad49ae2cdbf3413a
arun73822 <arun73822@gmail.com>
2022-11-10 11:59:34 +0530

    changed

--- src/Object_Detection/pipeline/data_ingestion.py
+++ src/Object_Detection/pipeline/data_ingestion.py
@@ -3,19 +3,19 @@ from Object_Detection.config.configuration import Configuration
 from Object_Detection.entity.config_artifact import Data_Ingestion_Artifact
 from Object_Detection.component.data_ingestion import Data_Ingestion
 
-class pipeline:
-    def __init__(self,config:Configuration) -> None:
-        self.config=config
-    
-    def start_data_ingestion(self):
-        try:
-            data_ingestion=Data_Ingestion(data_ingestion_config=self.config.get_data_ingestion_config())
-            return data_ingestion.initiate_data_ingestion()
-        except Exception as e:
-            raise e
-    
-    def run_pipeline(self):
-        try:
-            self.start_data_ingestion()
-        except Exception as e:
-            raise e
+STAGE_NAME="Data_Ingestion"
+
+def main():
+    config=Configuration()
+    data_ingestion_config=config.get_data_ingestion_config()
+    data_ingestion=Data_Ingestion(data_ingestion_config=data_ingestion_config)
+    data_ingestion.initiate_data_ingestion()
+
+if __name__=="__main__":
+    try:
+        logger.info(f"\n\n>>>>>> stage {STAGE_NAME} started <<<<<<")
+        main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
